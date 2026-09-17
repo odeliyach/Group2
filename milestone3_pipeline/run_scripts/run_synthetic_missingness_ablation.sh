f47aec3778e0d27c40e42c8c6ed612f5f937531f
liorpernik <liorpernik@tau.mail.ac.il>
2026-09-17 11:48:39 +0300

    time limit > 23h

--- milestone3_pipeline/run_scripts/run_synthetic_missingness_ablation.sh
+++ milestone3_pipeline/run_scripts/run_synthetic_missingness_ablation.sh
@@ -2,11 +2,11 @@
 #SBATCH --job-name=milestone3-synth-missingness
 #SBATCH --output=logs/m3_synth_missing_%A_%a.out
 #SBATCH --error=logs/m3_synth_missing_%A_%a.err
-#SBATCH --array=0-1
+#SBATCH --array=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=8G
-#SBATCH --time=01:30:00
+#SBATCH --time=23:00:00
 #SBATCH --partition=studentkillable
 #SBATCH --open-mode=truncate
 #
